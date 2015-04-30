f935d2949cb6d8f7a2aaf3bf3df303c71340aa0c
Jeremy Guillette <jaguillette@gmail.com>
2015-04-30 12:59:46 -0400

    renamed map interface, went on a commenting spree

--- index.php
+++ index.php
@@ -55,12 +55,13 @@
 		<!-- Leaflet Sidebar -->
 		<script type="text/javascript" src="js/leaflet-sidebar.js"></script>
 	<script type="text/javascript">
+	// Map creation
 	var map = L.map('map').setView([0, 0], 1);
 
+	// Adding tile layers
 	var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 	});
-
 	var Stamen_Watercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png', {
 		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 		subdomains: 'abcd',
@@ -68,16 +69,13 @@
 		maxZoom: 16,
 		ext: 'png'
 	});
-
 	var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 		attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 	});
-
 	var Esri_NatGeoWorldMap = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
 		attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
 		maxZoom: 16
 	});
-
 	var NASAGIBS_ViirsEarthAtNight2012 = L.tileLayer('http://map1.vis.earthdata.nasa.gov/wmts-webmerc/VIIRS_CityLights_2012/default/{time}/{tilematrixset}{maxZoom}/{z}/{y}/{x}.{format}', {
 		attribution: 'Imagery provided by services from the Global Imagery Browse Services (GIBS), operated by the NASA/GSFC/Earth Science Data and Information System (<a href="https://earthdata.nasa.gov">ESDIS</a>) with funding provided by NASA/HQ.',
 		bounds: [[-85.0511287776, -179.999999975], [85.0511287776, 179.999999975]],
@@ -87,7 +85,6 @@
 		time: '',
 		tilematrixset: 'GoogleMapsCompatible_Level'
 	});
-
 	var baseMaps = {
 		"Open Street Map": OpenStreetMap_Mapnik,
 		"Stamen Watercolor": Stamen_Watercolor,
@@ -95,16 +92,16 @@
 		"National Geographic World Map": Esri_NatGeoWorldMap,
 		"NASA Earth at Night": NASAGIBS_ViirsEarthAtNight2012
 	}
-
+	// Adding tile layer control
 	L.control.layers(baseMaps).addTo(map);
 
+	// Style definitions
 	var defaultStyle = {
 		"color": "#B80407",
 		"opacity": 0.5,
 		"weight": 3,
 		"fillOpacity": 0.1
 	};
-
 	var hoverStyle = {
 		"color": "#E1ED00",
 		"opacity": 0.8,
@@ -113,6 +110,7 @@
 	};
 
 	function highlightFeature(e) {
+		// Adds highlight styling, moves feature to back.
 		var layer = e.target;
 
 		dispBoxes.eachLayer(function(layer) {
@@ -128,43 +126,33 @@
 	}
 
 	function resetHighlight(e) {
+		// Resets highlighting
 		var layer = e.target;
 		layer.setStyle(defaultStyle);
 	}
 
 	function onEachFeature(feature,layer) {
-		// defining popup content for polygons. 
-		// Will eventually switch to sidebar, this is just easier
-		/*popupContent = "";
-		if (feature.properties && feature.properties.fname) {
-			popupContent += "<p>" + feature.properties.fname + "</p>";
-		}
-		if (feature.properties && feature.properties.collection) {
-			popupContent += "<p>" + feature.properties.collection + "</p>";
-		}
-		if (feature.properties && feature.properties.lat_extent) {
-			popupContent += "<p>" + feature.properties.lat_extent + "</p>";
-		}
-		if (feature.properties && feature.properties.lng_extent) {
-			popupContent += "<p>" + feature.properties.lng_extent + "</p>";
-		}
-		layer.bindPopup(popupContent);*/
 		// changing styling based on mouseover events
 		layer.on({
 			click: highlightFeature
 		});
+		// Assigning polygon IDs based on UNIQUE_ID in feature
 		layer._polygonId = feature.properties.UNIQUE_ID;
 	}
 
 	$.getJSON($('link[rel="polygons"]').attr("href"), function(data) {
+		// Getting subset of geoJSON to display based on current zoom level
 		var z = map.getZoom()
 		dispBoxes = L.geoJson(data, {
 			style: defaultStyle,
 			onEachFeature: onEachFeature,
 			filter: function(feature,layer) {
+				// This line determines display based on zoom level
+				// Gets the appropriate zoom level for the feature and compares it to current zoom level
 				return z==map.getBoundsZoom(L.geoJson(feature).getBounds())
 			}
 		});
+		// On zoom end, recalculates which features to display using same method as before.
 		map.on('zoomend', function(e) {
 			map.removeLayer(dispBoxes);
 			var z = map.getZoom();
@@ -172,15 +160,29 @@
 				style: defaultStyle,
 				onEachFeature: onEachFeature,
 				filter: function(feature,layer) {
+					// This line determines display based on zoom level
+					// Gets the appropriate zoom level for the feature and compares it to current zoom level
 					return z==map.getBoundsZoom(L.geoJson(feature).getBounds())
 				}
 			});
 			dispBoxes.addTo(map)
 		})
-		var allBoxes = L.geoJson(data, {onEachFeature: onEachFeature})
+
+		// Adds sidebar as a control
 		var sidebar = L.control.sidebar('sidebar').addTo(map);
+		
+		// Adds initial base layer
 		OpenStreetMap_Mapnik.addTo(map);
+
+		// Adds initial polygon layer, defined earlier based on initial zoom level
 		dispBoxes.addTo(map);
+
+		// Defines a variable containing all geoJSON features
+		// This will be used for zooming to polygons that are not currently displayed
+		var allBoxes = L.geoJson(data, {onEachFeature: onEachFeature})
+
+		// jQuery, on ID link click, map will zoom to polygon with corresponding ID
+		// Corresponding ID should also be highlighted
 		$(".idLink").on("click",function() {
 			var searchId=$(this).attr("id")
 			allBoxes.eachLayer(function(layer) {
