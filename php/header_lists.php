229ba8d4506368fb1ce4ab91eef6ee7881b6f850
Jeremy Guillette <jaguillette@gmail.com>
2015-05-15 13:03:54 -0400

    added relative links to php so that it works properly
    I had been using absolute paths, dependent on a network connection, which was silly

--- php/header_lists.php
+++ php/header_lists.php
@@ -1,31 +1,31 @@
 <?php
-#echo "Some PHP happened!<br/>";
+error_reporting(E_ALL);
 # The following creates an array called $csv_array based on an input csv.
 # The resulting array can be accessed by row and column name like so:
 # $csv_array[`row_number`][`column_name`]
-$input_csv = "http://localhost/harvardMapCollection/seaAtlas/php/atlas_metadata.csv";
+$input_csv = __DIR__."/atlas_metadata.csv";
 $csv_array = $fields = array(); $i = 0;
 $handle = @fopen($input_csv, "r");
 if ($handle) {
-    while (($row = fgetcsv($handle, 4096)) !== false) {
-    	# $fields holds the row names, and uses the first row of the CSV
-        if (empty($fields)) {
-            $fields = $row;
-            continue;
-        }
-        foreach ($row as $k=>$value) {
-        	# This is where the array values get set
-            $csv_array[$i][$fields[$k]] = $value;
-        }
-        $i++;
-    }
-    if (!feof($handle)) {
-        echo "Error: unexpected fgets() fail\n";
-    }
-    fclose($handle);
+	while (($row = fgetcsv($handle, 4096)) !== false) {
+		# $fields holds the row names, and uses the first row of the CSV
+		if (empty($fields)) {
+			$fields = $row;
+			continue;
+		}
+		foreach ($row as $k=>$value) {
+			# This is where the array values get set
+			$csv_array[$i][$fields[$k]] = $value;
+		}
+		$i++;
+	}
+	if (!feof($handle)) {
+		echo "Error: unexpected fgets() fail\n";
+	}
+	fclose($handle);
 }
 foreach ($csv_array as $index => $row) {
-	echo "<input id=\"".$row["IDENTIFIER"]."_checkbox\" type=\"checkbox\" class=\"filterControl\" value=\"".$row["IDENTIFIER"]."\"/>\n";
+	echo("<input id=\"".$row["IDENTIFIER"]."_checkbox\" type=\"checkbox\" class=\"filterControl\" value=\"".$row["IDENTIFIER"]."\"/>\n");
 	echo("\t\t\t\t\t<div id=\"currentViewContent\" class=\"collapsible collapseL1\">\n");
 	echo("\t\t\t\t\t\t<h2 id=\"".$row["IDENTIFIER"]."CurrentHeading\">\n");
 	echo("\t\t\t\t\t\t\t<span class=\"arrow arrow-r\"></span>\n");
