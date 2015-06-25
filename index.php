433c3446a37a6934fa46943b77f46bff3c25cdcd
Jeremy Guillette <jaguillette@gmail.com>
2015-06-25 11:18:02 -0400

    changed initially active tab

--- index.php
+++ index.php
@@ -40,16 +40,16 @@
 	<body>
 		<div id="sidebar" class="sidebar" style="background-image:url(bg-test.png)">
 			<ul class="sidebar-tabs" role="tablist">
-				<li id="bigListTab" class="active"><a href="#bigList" role="tab"><i class="fa fa-list" title="List of all sea charts available for display"></i><span class="tabDescription">Comprehensive List</span></a></li>
-				<li id="currentViewTab"><a href="#currentView" role="tab"><i class="fa fa-compass" title="List of sea charts in current view"></i><span class="tabDescription">Map Based List</span></a></li>
+				<li id="bigListTab"><a href="#bigList" role="tab"><i class="fa fa-list" title="List of all sea charts available for display"></i><span class="tabDescription">Comprehensive List</span></a></li>
+				<li id="currentViewTab" class="active"><a href="#currentView" role="tab"><i class="fa fa-compass" title="List of sea charts in current view"></i><span class="tabDescription">Map Based List</span></a></li>
 				<li id="selectionsTab"><a href="#selections"><i class="fa fa-star" title="List of charts that you've added to the current view"></i><span class="tabDescription">Charts Displayed</span></a></li>
 				<li id="helpTab"><a href="#help"><i class="fa fa-question" title="Help with using this interface"></i><span class="tabDescription">How to use this</span></a></li>
 			</ul>
 			<div class="sidebar-content">
-				<div id="bigList" class="sidebar-pane active">
+				<div id="bigList" class="sidebar-pane">
 					<?php include("php/big_list_headers.php"); ?>
 				</div>
-				<div id="currentView" class="sidebar-pane">
+				<div id="currentView" class="sidebar-pane active">
 					<?php include("php/current_view_headers.php"); ?>
 				</div>
 				<div id="selections" class="sidebar-pane">
